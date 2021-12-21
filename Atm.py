9dfd7939ec07ecb424df80777b6a4858e5c1ed4e
TheMinmusDerp <71770907+TheMinmusDerp@users.noreply.github.com>
2021-12-21 16:50:18 -0600

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,8 @@
+class atm(object):
+  def __init__(self,cardnumber,pinnumber):
+    self.cardnumber=cardnumber
+    self.pinnumber=pinnumber
+  def CashWithdrawl(amount):
+    print("cash removed from account, if the quantum tunneling worked you'll see a pile of cash in front of you")
+  def BalanceEnquiry():
+    print("Jeff Bezos has more than you and always will unless you rise up in revolution with your comrades")
